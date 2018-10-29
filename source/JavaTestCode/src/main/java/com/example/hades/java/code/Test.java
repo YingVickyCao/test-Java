ef0ebc54a5d38824f81c859bf91c308676a7e3c2
Vicky <ying.vicky.cao@outlook.com>
2018-10-29 11:16:52 +0800

    LinkedHashMap

---
+++ source/JavaTestCode/src/main/java/com/example/hades/java/code/Test.java
@@ -0,0 +1,27 @@
+package com.example.hades.java.code;
+
+public class Test {
+    static final int MAXIMUM_CAPACITY = 1 << 30;
+
+    public static void main(String[] args) {
+        System.out.println("ABC");
+        System.out.println(tableSizeFor(1000));
+    }
+
+    static final int tableSizeFor(int cap) {
+        int n = cap - 1;
+        System.out.println(n);
+        System.out.println(n >>> 1);
+        n |= n >>> 1;
+        System.out.println(n);
+        n |= n >>> 2;
+        System.out.println(n);
+        n |= n >>> 4;
+        System.out.println(n);
+        n |= n >>> 8;
+        System.out.println(n);
+        n |= n >>> 16;
+        System.out.println(n);
+        return (n < 0) ? 1 : (n >= MAXIMUM_CAPACITY) ? MAXIMUM_CAPACITY : n + 1;
+    }
+}
--- source/JavaTestCode/src/main/java/com/example/hades/java/demo/Test.java
+++
@@ -1,7 +0,0 @@
-package com.example.hades.java.demo;
-
-public class Test {
-    public static void main(String[] args){
-        System.out.println("ABC");
-    }
-}
