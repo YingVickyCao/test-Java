1aeb6c64bede3250af12067026fe1aafbcb415ad
Vicky <ying.vicky.cao@outlook.com>
2018-12-19 20:43:03 +0800

    goto

--- source/JavaTestCode/src/main/java/com/example/hades/java/code/_control_execute_process/Test.java
+++ source/JavaTestCode/src/main/java/com/example/hades/java/code/_control_execute_process/Test.java
@@ -2,7 +2,8 @@ package com.example.hades.java.code._control_execute_process;
 
 public class Test {
     public static void main(String[] args) {
-        testBreakAndContinue();
+//        testBreakAndContinue();
+        testGoto();
     }
 
     private static void testBreakAndContinue() {
@@ -15,6 +16,48 @@ public class Test {
                 continue;
             }
             System.out.print(i + "  ");
+        }
+    }
+
+    private static void testGoto() {
+        int i = 0;
+
+        outer:
+
+        for (; true; ) {
+            inner:
+
+            for (; i < 10; i++) {
+                System.out.println("i=" + i);
+                if (i == 2) {
+                    System.out.println("continue");
+                    continue;
+                }
+
+                if (i == 3) {
+                    System.out.println("break");
+                    i++;
+                    break;
+                }
+
+                if (i == 7) {
+                    System.out.println("continue outer");
+                    i++;
+                    continue outer; // break all iteration, and jump to label outer, begin iteration again
+                }
+
+                if (i == 8) {
+                    System.out.println("break outer");
+                    break outer; // break all iteration, and jump to label outer, never begin iteration again.
+                }
+
+                for (int k = 0; k < 5; k++) {
+                    if (k == 3) {
+                        System.out.println("continue inner");
+                        continue inner;
+                    }
+                }
+            }
         }
     }
 }
