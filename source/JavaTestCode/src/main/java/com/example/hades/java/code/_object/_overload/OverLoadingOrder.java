6c5c2631bcc745ffa0fd49a095e0af7234ef8402
Vicky <ying.vicky.cao@outlook.com>
2018-12-20 18:13:52 +0800

    5.2.2 涉及基本类型的重载

---
+++ source/JavaTestCode/src/main/java/com/example/hades/java/code/_object/_overload/OverLoadingOrder.java
@@ -0,0 +1,17 @@
+package com.example.hades.java.code._object._overload;
+
+public class OverLoadingOrder {
+    public static void main(String[] args) {
+        OverLoadingOrder overLoadingOrder = new OverLoadingOrder();
+        overLoadingOrder.f(1, "A");
+        overLoadingOrder.f("B", 2);
+    }
+
+    private void f(final int i, final String s) {
+        System.out.println(i + "," + s);
+    }
+
+    private void f(final String s, final int i) {
+        System.out.println(i + "," + s);
+    }
+}
---
+++ source/JavaTestCode/src/main/java/com/example/hades/java/code/_object/_overload/PrimitiveOverloading2.java
@@ -0,0 +1,20 @@
+package com.example.hades.java.code._object._overload;
+
+public class PrimitiveOverloading2 {
+    public static void main(String[] args) {
+        PrimitiveOverloading2 test = new PrimitiveOverloading2();
+
+        test.testLong();
+        System.out.println();
+    }
+
+    private void testLong() {
+        System.out.print("long:");
+        long x = 1;
+        f4((int) x);
+    }
+
+    private void f4(int x) {
+        System.out.print("f4(int)   ");
+    }
+}
