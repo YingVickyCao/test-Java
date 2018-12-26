9e9d3b1f7fda76df49da91f04286701684903388
Vicky <ying.vicky.cao@outlook.com>
2018-12-26 22:33:51 +0800

    6.3 接口和实现

--- source/JavaTestCode/src/main/java/com/example/hades/java/code/_object/_access_control/_private/B.java
+++ source/JavaTestCode/src/main/java/com/example/hades/java/code/_object/_access_control/_private/B.java
@@ -1,6 +1,7 @@
 package com.example.hades.java.code._object._access_control._private;
 
-public class B extends A {
+//public class B extends A {
+public class B {
 //    public B(int n) {
 //        super(n);
 //    }
