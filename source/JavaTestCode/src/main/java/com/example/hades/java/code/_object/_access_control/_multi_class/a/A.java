76998ae7db1ede0ed6eafde69e3f64563db8f3d8
Vicky <ying.vicky.cao@outlook.com>
2018-12-27 08:01:39 +0800

    6.5 总结

--- source/JavaTestCode/src/main/java/com/example/hades/java/code/_object/_access_control/_multi_class/a/A.java
+++ source/JavaTestCode/src/main/java/com/example/hades/java/code/_object/_access_control/_multi_class/a/A.java
@@ -2,19 +2,39 @@ package com.example.hades.java.code._object._access_control._multi_class.a;
 
 public class A {
     private int a;
+    private H h3;
 
-    A(int a) {
+//    A(int a) {
+    public A(int a) {
         this.a = a;
+        h3 = new H("H3");
+        System.out.println(h3.s);
     }
 
-    int getA() {
+//    int getA() {
+    public int getA() {
+        System.out.println(h3.s);
         return a;
     }
 
-    class M {
-        int m;
+//    H getH() {
+    public H getH() {
+        return h3;
+    }
 
-        public M() {
+//    class H {
+    public class H {
+        String s;
+//        private String s;
+
+//        H(String s) {
+        public H(String s) {
+            this.s = s;
+        }
+
+//        String getH() {
+        public String getH() {
+            return s;
         }
     }
 }
