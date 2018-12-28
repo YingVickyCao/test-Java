4ef22d66b30c8cd656f7191bf915d81eed94be88
Vicky <ying.vicky.cao@outlook.com>
2018-12-28 09:26:18 +0800

    7.4.1 确保正确清理

---
+++ source/JavaTestCode/src/main/java/com/example/hades/java/code/_object/_cleanup/Circle.java
@@ -0,0 +1,19 @@
+package com.example.hades.java.code._object._cleanup;
+
+public class Circle extends Shape {
+    private static final String TAG = Circle.class.getSimpleName();
+
+    Circle(int size) {
+        super(size);
+        System.out.println(TAG + " Constructor ---------->");
+        System.out.println(TAG + " Constructor <----------");
+        System.out.println();
+    }
+
+    void clean() {
+        System.out.println(TAG + " clean() ---------->");
+        super.clean();
+        System.out.println(TAG + " clean() <----------");
+        System.out.println();
+    }
+}
