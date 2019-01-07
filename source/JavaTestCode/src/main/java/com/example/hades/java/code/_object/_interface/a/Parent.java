def9f0ef16c5989339f33174299db8b7af5ad442
Vicky <ying.vicky.cao@outlook.com>
2019-01-07 13:03:02 +0800

    9.2 接口

---
+++ source/JavaTestCode/src/main/java/com/example/hades/java/code/_object/_interface/a/Parent.java
@@ -0,0 +1,16 @@
+package com.example.hades.java.code._object._interface.a;
+
+public abstract class Parent {
+    private static final String TAG = Parent.class.getSimpleName();
+
+    public Parent() {
+        System.out.println(TAG + " Constructor,START");
+        print();
+        System.out.println(TAG + " Constructor,END");
+        System.out.println();
+    }
+
+    abstract String what();
+
+    abstract void print();
+}
