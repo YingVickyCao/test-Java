f1f6c2d1795b5dcba45575c1ab5f312bf0cbe745
Vicky <ying.vicky.cao@outlook.com>
2020-02-28 10:29:08 +0800

    Create test.java

---
+++ src/main/java/com/example/hades/java/code/_condition/test.java
@@ -0,0 +1,16 @@
+package com.example.hades.java.code._condition;
+
+public class test {
+
+    public static void main(String[] args) {
+        int num = 18;
+        // 18 >= 17
+        if (num >= 17) {
+            System.out.println(num + " >= 17");
+        } else if (num >= 16) {
+            System.out.println(num + " >= 16");
+        } else {
+            System.out.println("Other");
+        }
+    }
+}
