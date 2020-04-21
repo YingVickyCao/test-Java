80e3ebd9b24bbd93e1eeee5e93bbca1ac7f088e5
Vicky <ying.vicky.cao@outlook.com>
2020-04-21 21:35:35 +0800

    Dec

---
+++ src/main/java/com/hades/example/java/security/Test.java
@@ -0,0 +1,25 @@
+package com.hades.example.java.security;
+
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[]args){
+        Test test = new Test();
+        test.testDec();
+    }
+
+    private void testDec(){
+        System.out.println("Dec, start");
+        Des des = new Des();
+
+        byte[] encrypted = des.encrypt(Des.TO_ENCRYPT_CONTENT.getBytes()); // "Hi123"
+        System.out.println(Arrays.toString(encrypted)); // [121, 53, 22, 92, -126, -93, -79, 94]
+
+        byte[] decrypted = des.decrypt(Des.TO_DECRYPT_CONTENT);// [121, 53, 22, 92, -126, -93, -79, 94]
+        System.out.println(Arrays.toString(decrypted)); // [72, 105, 49, 50, 51]
+        System.out.println(new String(decrypted));  // Hi123
+        //        System.out.println((char)72); // H
+
+        System.out.println("Dec, end");
+    }
+}
