f98c3ca2015f6b3c2313488e4d7c6c8bb33e92ff
Vicky <ying.vicky.cao@outlook.com>
2023-02-15 21:54:20 +0800

    refactor

--- src/main/java/com/hades/java/example/_generic_type/_6_Use_Wildcard_extends.java
+++ src/main/java/com/hades/java/example/_generic_type/_6_Use_Wildcard_extends.java
@@ -1,9 +1,9 @@
 package com.hades.java.example._generic_type;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
+/**
+ * 通配符 ( ? extends 父类)：
+ * 表示只能接收父类以及它的子类且接收后只能使用父类中方法。
+ */
 public class _6_Use_Wildcard_extends {
     public static void main(String[] args) {
         _6_Use_Wildcard_extends example = new _6_Use_Wildcard_extends();
@@ -11,160 +11,70 @@ public class _6_Use_Wildcard_extends {
     }
 
     private void test() {
-        {
-            {
-                List<Integer> list = new ArrayList<>();
-                list.add(1);
-                list.add(2);
-                list.add(3);
-                printList(list);
-            }
+        BusinessMan<Fruit> v1 = new BusinessMan<>();
+        v1.setFruit(new Fruit());
+        v1.fruit.name();
+        System.out.println();
 
-            {
-                List<String> list = new ArrayList<>();
-                list.add("A");
-                list.add("B");
-                list.add("C");
-                printList(list);
-            }
+        BusinessMan<Apple> v2 = new BusinessMan<>();
+        v2.setFruit(new Apple());
+        v2.fruit.name();
+        v2.fruit.amApple();
+        System.out.println();
 
-            {
-                List list = new ArrayList<>();
-                list.add(1);
-                list.add("A");
-                printList(list);
-            }
-        }
-        {
-            {
-                List<Integer> list = new ArrayList<>();
-                list.add(1);
-                list.add(2);
-                list.add(3);
+        BusinessMan<Orange> v3 = new BusinessMan<>();
+        v3.setFruit(new Orange());
+        v3.fruit.name();
+        v3.fruit.amOrange();
+        System.out.println();
 
-                /**
-                 * Error : Compile error
-                 *  Required type:List<Object>
-                 * Provided:List<Integer>
-                 */
-//                printList2(list);
-            }
-
-            {
-                List<Object> list = new ArrayList<>();
-                list.add(1);
-                list.add(2);
-                list.add(3);
-                printList2(list);
-            }
-
-            {
-                List<Object> list = new ArrayList<>();
-                list.add("A");
-                list.add("B");
-                list.add("C");
-                printList2(list);
-            }
-
-            {
-                List<Object> list = new ArrayList<>();
-                list.add(1);
-                list.add("A");
-                printList(list);
-            }
-        }
-
-        {
-
-            {
-                List<Integer> list = new ArrayList<>();
-                list.add(1);
-                list.add(2);
-                list.add(3);
-                printList3(list);
-            }
-
-            {
-                List<String> list = new ArrayList<>();
-                list.add("A");
-                list.add("B");
-                list.add("C");
-                printList3(list);
-            }
-
-            {
-                List<Object> list = new ArrayList<>();
-                list.add(1);
-                list.add("A");
-                printList(list);
-            }
-
-            {
-                List<?> list = new ArrayList<String>();
-                /**
-                 * Error : Compile error
-                 * Required type:capture of ?
-                 * Provided:Object
-                 */
-                // list.add(new Object());
-
-                list.add(null);
-
-                /**
-                 * Error : Compile error
-                 * Required type:capture of ?
-                 * Provided:String
-                 */
-//                list.add("ag");
-            }
-        }
-
-
+        sale(v1);
+        sale(v2);
+        sale(v3);
     }
 
-    /**
-     * 不使用泛型
-     *
-     * @param list List<任何类类型> / List, list可以接收元素
+    /*
+            Fruit
+           /     \
+       Apple       Orange
      */
-    private void printList(List list) {
-        Iterator iterator = list.iterator();
-        for (int i = 0; i < list.size(); i++) {
-            System.out.println(iterator.next());
+
+    class Fruit {
+        public void name() {
+            System.out.println(this.getClass().getSimpleName());
         }
     }
 
-    /**
-     * 不使用泛型
-     *
-     * @param list 只能传 List<Object>，list可以接收元素
-     */
-    private void printList2(List<Object> list) {
-        for (Object item : list) {
-            System.out.println(item);
+    class Apple extends Fruit {
+        public void amApple() {
+            System.out.println("I am apple");
         }
     }
 
-    /**
-     * 使用通配符?
-     * <p>
-     * 通配符？:
-     * 表示该集合存储的元素类型未知，可以是任何类型。往集合中加入元素需要是一个未知元素类型的子类型，正因为该集合存储的元素类型未知，
-     * 所以我们没法向该集合中添加任何元素。唯一的例外是null，因为null是所有类型的子类型，所以尽管元素类型不知道，但是null一定是它的子类型。
-     * 因此：通配符？, 只能用在一个函数的形参，不能用在实惨。否则，只能添加null，不能添加除了null之前的任何元素。
-     *
-     * 即使如此，实际开发中也不会使用List<?>。因为数组的本身含义是一组相同的类型，没有必要使用List<?>，而是使用List<具体类型>
-     *
-     * @param list List<任何类类型>, list可以接收元素
-     */
-    private void printList3(List<?> list) {
-        Iterator<?> iterator = list.iterator();
-        for (int i = 0; i < list.size(); i++) {
-            System.out.println(iterator.next());
+    class Orange extends Fruit {
+        public void amOrange() {
+            System.out.println("I am Orange");
+        }
+    }
+
+    class BusinessMan<T> {
+        private T fruit;
+
+        public void setFruit(T fruit) {
+            this.fruit = fruit;
         }
 
-        for (Object item : list) {
-            System.out.println(item);
+        public T getFruit() {
+            return fruit;
         }
+    }
+
+    private void sale(BusinessMan<? extends Fruit> businessMan) {
+        /**
+         * BusinessMan<Orange> 传入后，not able to invoke amOrange()，only can invoke name()
+         * BusinessMan<Apple>  传入后，not able to invoke amApple()， only can invoke name()
+         *
+         */
+        businessMan.getFruit().name();
     }
 }
--- src/main/java/com/hades/java/example/_generic_type/_7_Use_Wildcard_super.java
+++ src/main/java/com/hades/java/example/_generic_type/_7_Use_Wildcard_super.java
@@ -1,9 +1,5 @@
 package com.hades.java.example._generic_type;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
 public class _7_Use_Wildcard_super {
     public static void main(String[] args) {
         _7_Use_Wildcard_super example = new _7_Use_Wildcard_super();
@@ -11,160 +7,87 @@ public class _7_Use_Wildcard_super {
     }
 
     private void test() {
-        {
-            {
-                List<Integer> list = new ArrayList<>();
-                list.add(1);
-                list.add(2);
-                list.add(3);
-                printList(list);
-            }
+        BusinessMan<Fruit> v1 = new BusinessMan<>();
+        v1.setFruit(new Fruit());
+        v1.fruit.name();
+        System.out.println();
 
-            {
-                List<String> list = new ArrayList<>();
-                list.add("A");
-                list.add("B");
-                list.add("C");
-                printList(list);
-            }
+        BusinessMan<Apple> v2 = new BusinessMan<>();
+        v2.setFruit(new Apple());
+        v2.fruit.name();
+        v2.fruit.amApple();
+        System.out.println();
 
-            {
-                List list = new ArrayList<>();
-                list.add(1);
-                list.add("A");
-                printList(list);
-            }
-        }
-        {
-            {
-                List<Integer> list = new ArrayList<>();
-                list.add(1);
-                list.add(2);
-                list.add(3);
+        BusinessMan<Orange> v3 = new BusinessMan<>();
+        v3.setFruit(new Orange());
+        v3.fruit.name();
+        v3.fruit.amOrange();
+        System.out.println();
 
-                /**
-                 * Error : Compile error
-                 *  Required type:List<Object>
-                 * Provided:List<Integer>
-                 */
-//                printList2(list);
-            }
+        BusinessMan<SmallOrange> v4 = new BusinessMan<>();
+        v4.setFruit(new SmallOrange());
+        v4.fruit.name();
+        v4.fruit.amOrange();
+        v4.fruit.amSmallOrange();
+        System.out.println();
 
-            {
-                List<Object> list = new ArrayList<>();
-                list.add(1);
-                list.add(2);
-                list.add(3);
-                printList2(list);
-            }
-
-            {
-                List<Object> list = new ArrayList<>();
-                list.add("A");
-                list.add("B");
-                list.add("C");
-                printList2(list);
-            }
-
-            {
-                List<Object> list = new ArrayList<>();
-                list.add(1);
-                list.add("A");
-                printList(list);
-            }
-        }
-
-        {
-
-            {
-                List<Integer> list = new ArrayList<>();
-                list.add(1);
-                list.add(2);
-                list.add(3);
-                printList3(list);
-            }
-
-            {
-                List<String> list = new ArrayList<>();
-                list.add("A");
-                list.add("B");
-                list.add("C");
-                printList3(list);
-            }
-
-            {
-                List<Object> list = new ArrayList<>();
-                list.add(1);
-                list.add("A");
-                printList(list);
-            }
-
-            {
-                List<?> list = new ArrayList<String>();
-                /**
-                 * Error : Compile error
-                 * Required type:capture of ?
-                 * Provided:Object
-                 */
-                // list.add(new Object());
-
-                list.add(null);
-
-                /**
-                 * Error : Compile error
-                 * Required type:capture of ?
-                 * Provided:String
-                 */
-//                list.add("ag");
-            }
-        }
-
-
+        sale(v1);
+        sale(v2);
+        sale(v3);
+        sale(v4);
     }
 
-    /**
-     * 不使用泛型
-     *
-     * @param list List<任何类类型> / List, list可以接收元素
-     */
-    private void printList(List list) {
-        Iterator iterator = list.iterator();
-        for (int i = 0; i < list.size(); i++) {
-            System.out.println(iterator.next());
+    /*
+                Fruit
+               /     \
+           Apple     Orange
+                        |
+                     SmallOrange
+
+         */
+    class Fruit {
+        public void name() {
+            System.out.println(this.getClass().getSimpleName());
         }
     }
 
-    /**
-     * 不使用泛型
-     *
-     * @param list 只能传 List<Object>，list可以接收元素
-     */
-    private void printList2(List<Object> list) {
-        for (Object item : list) {
-            System.out.println(item);
+    class Apple extends Fruit {
+        public void amApple() {
+            System.out.println("I am apple");
         }
     }
 
-    /**
-     * 使用通配符?
-     * <p>
-     * 通配符？:
-     * 表示该集合存储的元素类型未知，可以是任何类型。往集合中加入元素需要是一个未知元素类型的子类型，正因为该集合存储的元素类型未知，
-     * 所以我们没法向该集合中添加任何元素。唯一的例外是null，因为null是所有类型的子类型，所以尽管元素类型不知道，但是null一定是它的子类型。
-     * 因此：通配符？, 只能用在一个函数的形参，不能用在实惨。否则，只能添加null，不能添加除了null之前的任何元素。
-     *
-     * 即使如此，实际开发中也不会使用List<?>。因为数组的本身含义是一组相同的类型，没有必要使用List<?>，而是使用List<具体类型>
-     *
-     * @param list List<任何类类型>, list可以接收元素
-     */
-    private void printList3(List<?> list) {
-        Iterator<?> iterator = list.iterator();
-        for (int i = 0; i < list.size(); i++) {
-            System.out.println(iterator.next());
+    class Orange extends Fruit {
+        public void amOrange() {
+            System.out.println("I am Orange");
+        }
+    }
+
+
+    class SmallOrange extends Orange {
+        public void amSmallOrange() {
+            System.out.println("I am SmallOrange");
+        }
+    }
+
+    class BusinessMan<T> {
+        private T fruit;
+
+        public void setFruit(T fruit) {
+            this.fruit = fruit;
         }
 
-        for (Object item : list) {
-            System.out.println(item);
+        public T getFruit() {
+            return fruit;
         }
+    }
+
+    private void sale(BusinessMan<? super Orange> businessMan) {
+        /**
+         * BusinessMan<Orange> 传入后，not able to invoke amOrange()，only can invoke name()
+         * BusinessMan<Apple>  传入后，not able to invoke amApple()， only can invoke name()
+         *
+         */
+        businessMan.getFruit();
     }
 }
